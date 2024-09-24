6109d997f5abde0eaa4f9ae8bb3bd8a2189334e0
Dawid Ratajczak <dawid.ratajczak06@gmail.com>
2024-09-24 12:03:35 +0200

    move items to mc

--- src/main/java/pl/creazy/creazylib/util/mc/Mc.java
+++ src/main/java/pl/creazy/creazylib/util/mc/Mc.java
@@ -1,10 +1,14 @@
 package pl.creazy.creazylib.util.mc;
 
 import lombok.experimental.UtilityClass;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -82,5 +86,45 @@ public class Mc {
 
   public boolean hasEmptySlot(@NotNull InventoryHolder holder) {
     return holder.getInventory().firstEmpty() != -1;
+  }
+
+  public static boolean isEmpty(@Nullable ItemStack item) {
+    return item == null || item.getType() == Material.AIR || item.getItemMeta() == null;
+  }
+
+  @SuppressWarnings("DataFlowIssue")
+  public static int getEnchantLevel(@Nullable ItemStack item, @NotNull Enchantment enchant) {
+    if (isEmpty(item)) {
+      return 0;
+    }
+    return item.getItemMeta().getEnchantLevel(enchant);
+  }
+
+  public static @Nullable ItemStack getRod(@NotNull Player player) {
+    var main = player.getInventory().getItemInMainHand();
+    if (main.getType() == Material.FISHING_ROD) {
+      return main;
+    }
+    var off = player.getInventory().getItemInOffHand();
+    if (off.getType() == Material.FISHING_ROD) {
+      return off;
+    }
+    return null;
+  }
+
+  public static boolean isFish(@NotNull ItemStack item) {
+    var type = item.getType();
+    return type == Material.SALMON || type == Material.COD || type == Material.TROPICAL_FISH;
+  }
+
+  public static int getEnchantLevel(@NotNull Player player, @NotNull Enchantment enchant) {
+    return getEnchantLevel(player.getInventory().getItemInMainHand(), enchant);
+  }
+
+  public static void replace(@NotNull ItemStack item, @NotNull ItemStack replacer) {
+    item.setItemMeta(replacer.getItemMeta());
+    item.setType(replacer.getType());
+    item.setData(replacer.getData());
+    item.setAmount(replacer.getAmount());
   }
 }
